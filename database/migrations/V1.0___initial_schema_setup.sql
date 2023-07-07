4e091141bd73545af27b16f39cda2d3264aa204c
Thomas McKee <thomas.mckee@BEL-MC-230631A.local>
2023-07-07 10:25:39 +0100

    updated postgre

--- database/migrations/V1.0___initial_schema_setup.sql
+++ database/migrations/V1.0___initial_schema_setup.sql
@@ -1,4 +1,4 @@
-CREATE TABLE users(
+CREATE TABLE IF NOT EXISTS users(
     id SERIAL NOT NULL PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL,
     last_name VARCHAR(255) NOT NULL,
@@ -6,17 +6,17 @@ CREATE TABLE users(
     email VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE syndicate_roles(
+CREATE TABLE IF NOT EXISTS syndicate_roles(
     id SERIAL NOT NULL PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE syndicate_types(
+CREATE TABLE IF NOT EXISTS syndicate_types(
     id SERIAL NOT NULL constraint syndicate_types_pk PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE syndicates(
+CREATE TABLE IF NOT EXISTS syndicates(
     id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     description TEXT,
@@ -26,7 +26,7 @@ CREATE TABLE syndicates(
  CONSTRAINT FK_syndicate_type_syndicates  FOREIGN KEY syndicate_type_id REFERENCES syndicate_types(id)
 );
 
-CREATE TABLE user_syndicates(
+CREATE TABLE IF NOT EXISTS user_syndicates(
     id SERIAL PRIMARY KEY,
     created_date TIMESTAMP,
     start_date TIMESTAMP,
@@ -36,47 +36,47 @@ CREATE TABLE user_syndicates(
    CONSTRAINT fk_sydicate_role_user_syndicates FOREIGN KEY syndicate_role_id REFERENCES syndicate_roles(id)
 );
 
-CREATE TABLE user_syndicate_reviews(
+CREATE TABLE IF NOT EXISTS user_syndicate_reviews(
     id SERIAL PRIMARY KEY,
     created_date DATE,
     title VARCHAR(255),
     content TEXT
 );
 
-CREATE TABLE games(
+CREATE TABLE IF NOT EXISTS games(
     id SERIAL PRIMARY KEY,
     name VARCHAR(255),
     date TIMESTAMP,
     reward FLOAT
 );
 
-CREATE TABLE boards(
+CREATE TABLE IF NOT EXISTS boards(
     id SERIAL PRIMARY KEY,
     board_title VARCHAR(255),
   CONSTRAINT FK_syndicate_boards FOREIGN KEY syndicate_id REFERENCES syndicates(id)
 );
 
-CREATE TABLE messages(
+CREATE TABLE IF NOT EXISTS messages(
     id SERIAL PRIMARY KEY,
     message TEXT,
   constraint FK_messages_users  FOREIGN KEY user_id REFERENCES users(id)
 );
 
-CREATE TABLE draws(
+CREATE TABLE IF NOT EXISTS draws(
     id SERIAL PRIMARY KEY,
     draw_date TIMESTAMP,
    CONSTRAINT FK_draws_game FOREIGN KEY  game_id REFERENCES games(id),
     CONSTRAINT FK_draws_boardsFOREIGN KEY board_id REFERENCES boards(id)
 );
 
-CREATE TABLE tickets(
+CREATE TABLE IF NOT EXISTS tickets(
     id SERIAL PRIMARY KEY,
     ticket_code VARCHAR(255),
    CONSTRAINT FK_tickets_draws FOREIGN KEY draw_id REFERENCES draws(id),
    CONSTRAINT FK_syndicate_draws FOREIGN KEY syndicate_id REFERENCES syndicates(id)
 );
 
-CREATE TABLE outcomes(
+CREATE TABLE IF NOT EXISTS outcomes(
     id SERIAL PRIMARY KEY,
     result VARCHAR(255),
     reward FLOAT,
