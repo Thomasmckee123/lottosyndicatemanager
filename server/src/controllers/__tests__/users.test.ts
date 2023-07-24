64fd65ea76efb44b0ec7799ba7b861aa5b11dcd9
Thomas McKee <thomas.mckee@bel-mc-230631b.ad.unosquare.com>
2023-07-24 09:35:47 +0100

    controller test

--- server/src/controllers/__tests__/users.test.ts
+++ server/src/controllers/__tests__/users.test.ts
@@ -66,3 +66,101 @@ describe("GET /users", () => {
         );
       });
     });
+
+    describe("getUserById", () => {
+        it("returns status code `200` and an array of users", async () => {
+          const request = httpMocks.createRequest({
+            method: "GET",
+            url: "/api/users/1",
+            params: {
+              userId: 1,
+            },
+          });
+          const response: MockResponse<Response> = createResponse();
+          const returnValue = {
+            id: 1,
+            first_name: "Thomas",
+            last_name: "mckee",
+            email: "JohnSmith@example.com",
+        
+          };
+          when(UserService.getUserById)
+            .calledWith(1)
+            .mockReturnValueOnce(Promise.resolve(returnValue));
+    
+          await UserController.getUserById(request, response);
+    
+          expect(response._getStatusCode()).toEqual(StatusCodes.OK);
+          expect(response._getJSONData()).toEqual(returnValue);
+        });
+        //negative testing
+        it("returns status code `404` if an error occurs", async () => {
+          const request = httpMocks.createRequest({
+            method: "GET",
+            url: "/api/users/",
+            params: {
+              userId: 1,
+            },
+          });
+          const response: MockResponse<Response> = createResponse();
+          await UserController.getUserById(request, response);
+    
+          expect(response._getStatusCode()).toEqual(
+            StatusCodes.NOT_FOUND
+          );
+        });
+      });
+    
+  //testing making user syndicate
+  describe("createUserSyndicate", () => {
+    const createBody = {
+     start_date: new Date(),
+     user_id: 1,
+     user_syndicate_id: 1,
+     role_id: 1
+    };
+    const invalidCreateBody = {
+        start_date: new Date(),
+     role_id: 99
+    };
+    it("returns status code `200` if user syndciate is successfully created", async () => {
+      const request = httpMocks.createRequest({
+        method: "POST",
+        url: "/1/syndicates/1",
+        body: createBody,
+      });
+      const response: MockResponse<Response> = createResponse();
+      const returnValue = {
+        start_date: new Date(),
+
+        role_id: 9999
+      };
+      when(UserService.createUserSyndicate)
+        .calledWith(createBody)
+        .mockReturnValueOnce(Promise.resolve(new Date()));
+
+      await UserController.createUserSyndicate(request, response);
+
+      expect(response._getStatusCode()).toEqual(StatusCodes.OK);
+    });
+    it("returns status code `400` if an error occurs", async () => {
+      const request = httpMocks.createRequest({
+        method: "POST",
+        url: "/1/syndicates/",
+        body: invalidCreateBody,
+      });
+      const response: MockResponse<Response> = createResponse();
+
+      when(UserService.createUserSyndicate)
+    .calledWith(invalidCreateBody)
+    .mockImplementationOnce(() => {
+      throw Error("Error getting user by id");
+    });
+
+      await UserController.createUserSyndicate(request, response);
+
+      expect(response._getStatusCode()).toEqual(
+        StatusCodes.INTERNAL_SERVER_ERROR
+      );
+    });
+  });
