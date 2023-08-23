45298272b31a94bc849ade43c7d9375cf3c09c60
Conor Mullan <cmullan@foundationmedicine.com>
2023-08-23 13:12:24 +0100

    Update gameTypes.ts

--- server/src/services/gameTypes.ts
+++ server/src/services/gameTypes.ts
@@ -4,86 +4,84 @@ import { IGameTypes } from "../interfaces/games";
 import { idText } from "typescript";
 
 const getAll = async () => {
-    try {
-        const allGames = await prisma.game_types.findMany({
-            select: {
-                id: true,
-                name: true,
-                draw_date: true,
-                reward: true,
-                image: true,
-                ticket_cost:true,
-            }
-        });
-        
-        const modifiedGameTypes: IGameTypes[] = allGames.map((x) => ({
-            id: x.id,
-            name: x.name,
-            drawDate: x.draw_date,
-            reward: x.reward,
-            image: x.image,
-            ticketCost:x.ticket_cost
-        }));
-        const filteredgameTypes = modifiedGameTypes?.filter((modifiedGameTypes) => modifiedGameTypes.name!== "game ended")
-        return filteredgameTypes;
-    } catch (error) {
-        console.error("Error fetching all games:", error);
-        return null;
-    }
+  try {
+    const allGames = await prisma.game_types.findMany({
+      select: {
+        id: true,
+        name: true,
+        draw_date: true,
+        reward: true,
+        image: true,
+        ticket_cost: true,
+      },
+    });
 
+    const modifiedGameTypes: IGameTypes[] = allGames.map((x) => ({
+      id: x.id,
+      name: x.name,
+      drawDate: x.draw_date,
+      reward: x.reward,
+      image: x.image,
+      ticketCost: x.ticket_cost,
+    }));
+    const filteredgameTypes = modifiedGameTypes?.filter(
+      (modifiedGameTypes) => modifiedGameTypes.name !== "game ended"
+    );
+    return filteredgameTypes;
+  } catch (error) {
+    console.error("Error fetching all games:", error);
+    return null;
+  }
 };
 
-const getGameTypeById = async(gameTypeId: number) =>{
-let gameTypeById = await  prisma.game_types.findUnique({
-  where:{id:gameTypeId},
-  select: {
-    id: true,
-    name: true,
-    draw_date: true,
-    reward: true,
-    image: true,
-    ticket_cost:true,
-}
-})
-if(gameTypeById){
-  const newModifiedGameTypes :IGameTypes={
-    id: gameTypeById.id,
-    name: gameTypeById?.name,
-    drawDate: gameTypeById?.draw_date,
-    reward: gameTypeById?.reward,
-    image:gameTypeById?.image,
-    ticketCost:gameTypeById?.ticket_cost
-  }
+const getGameTypeById = async (gameTypeId: number) => {
+  let gameTypeById = await prisma.game_types.findUnique({
+    where: { id: gameTypeId },
+    select: {
+      id: true,
+      name: true,
+      draw_date: true,
+      reward: true,
+      image: true,
+      ticket_cost: true,
+    },
+  });
+  if (gameTypeById) {
+    const newModifiedGameTypes: IGameTypes = {
+      id: gameTypeById.id,
+      name: gameTypeById?.name,
+      drawDate: gameTypeById?.draw_date,
+      reward: gameTypeById?.reward,
+      image: gameTypeById?.image,
+      ticketCost: gameTypeById?.ticket_cost,
+    };
 
-return newModifiedGameTypes;
-}else{
-  return null
-}
-}
+    return newModifiedGameTypes;
+  } else {
+    return null;
+  }
+};
 /**
- * 
+ *
  * @param gameTypes updating the gameTypes, archiving them
- * @returns 
+ * @returns
  */
 async function updateDates(gameTypes: any) {
-    let updateGameTypes;
-    try {
-      updateGameTypes = await prisma.game_types.update({
-        where: {
-          id: gameTypes.gameTypeId,
-        },
-        data: {
-     draw_date: gameTypes.drawDate
-        },          
-
-      });
-    } catch (error) {
-      console.log(error);
-    }
-    return updateGameTypes;
+  let updateGameTypes;
+  try {
+    updateGameTypes = await prisma.game_types.update({
+      where: {
+        id: gameTypes.gameTypeId,
+      },
+      data: {
+        draw_date: gameTypes.drawDate,
+      },
+    });
+  } catch (error) {
+    console.log(error);
   }
+  return updateGameTypes;
+}
 
-
-
-const GameTypeService ={ getAll, getGameTypeById, updateDates};
-export{GameTypeService}
+const GameTypeService = { getAll, getGameTypeById, updateDates };
+export { GameTypeService };
