9726c0b52c06154cebe6fe9f6d3bc0831519e336
Thomasmckee123 <thomas.mckee@unosquare.com>
2023-08-25 13:29:04 +0100

    updates

--- database/migrations/local data seed/V1000.1___local_data_seed.sql
+++ database/migrations/local data seed/V1000.1___local_data_seed.sql
@@ -1,50 +1,206 @@
--- INSERT INTO users (first_name, last_name, password, email) VALUES
--- ('Thomas', 'Mckee', 'qwerty12', 'thomasmckee12345@gmail.com'),
--- ('Jake', 'jones', 'w13c81', 'jjones23@gmail.com'),
--- ('Hannah', 'McConnell', 'niceHouse23', 'Hannah03@yahoo.com');
+INSERT INTO
+    users (
+        first_name,
+        last_name,
+        image,
+        password,
+        email,
+        balance
+    )
+VALUES (
+        'John',
+        'Doe',
+        'image',
+        'h4x0r123',
+        'JohnDoe@Gmail.com',
+        0
+    ), (
+        'Jane',
+        'Smith',
+        'image',
+        'al0ha123',
+        'JaneSmith@Gmail.com',
+        0
+    ), (
+        'Charlie',
+        'Brown',
+        'image',
+        'peanut123',
+        'CharlieBrown@Gmail.com',
+        0
+    );
 
--- INSERT INTO syndicates (created_date, name, description, avatar, owner_id) VALUES
--- ('2023-07-05', 'Thomas syndicate', 'I want people I know', 'h124ha1.jpg', 1),
--- ('2023-07-10', 'Pink Floyd', 'We just like pink floyd', '38Pb.jpg', 2),
--- ('2023-07-02', 'amazing group', 'we do everything over here', 'c23jauhe.jpg', 3);
+INSERT INTO
+    syndicates (
+        created_date,
+        name,
+        description,
+        avatar,
+        owner_id
+    )
+VALUES (
+        '2023-07-10',
+        'The Thunderbolts',
+        'For those who strike like lightning!',
+        'https://lottosyndicatebucket.s3.eu-west-1.amazonaws.com/syndicates/1/background+image+1.jpeg',
+        1
+    ), (
+        '2023-07-11',
+        'The Silent Shadows',
+        'We move in silence, we win in shadows.',
+        'https://lottosyndicatebucket.s3.eu-west-1.amazonaws.com/syndicates/2/background+image+2.webp',
+        2
+    ), (
+        '2023-07-12',
+        'Code Warriors',
+        'Battling bugs, one line of code at a time.',
+        'https://lottosyndicatebucket.s3.eu-west-1.amazonaws.com/syndicates/3/background+image+3.jpeg',
+        3
+    );
 
--- INSERT INTO roles (name) VALUES
--- ('Leader'),
--- ('Member'),
--- ('Moderator');
+INSERT INTO roles (name)
+VALUES ('Leader'), ('Member'), ('Moderator'), ('buyer');
 
--- INSERT INTO user_syndicates (start_date, user_id, syndicate_id, role_id) VALUES
--- ('2023-07-13', 1, 1, 1),
--- ('2023-07-14', 2, 2, 1),
--- ('2023-07-15', 3, 3, 1);
+INSERT INTO
+    user_syndicates (
+        start_date,
+        user_id,
+        syndicate_id,
+        role_id
+    )
+VALUES ('2023-07-13', 1, 1, 1), ('2023-07-14', 2, 2, 1), ('2023-07-15', 3, 3, 1);
 
--- INSERT INTO games (name, draw_date, reward, required_ticket_number, user_syndicate_id) VALUES
--- ('powerball', '2023-07-16', 50000.0, 1, 1),
--- ('national Lottery', '2023-07-17', 75000.0, 7, 2),
--- ('German Lottery', '2023-07-18', 100000.0, 8, 3);
+INSERT INTO
+    game_types(
+        name,
+        draw_date,
+        reward,
+        image,
+        ticket_cost
+    )
+VALUES (
+        'euro millions',
+        '2023-08-18',
+        43000000.0,
+        'https://lottosyndicatebucket.s3.eu-west-1.amazonaws.com/games/euromillions.jpeg',
+        2.50
+    ), (
+        'set for life',
+        '2023-08-17',
+        10000.0,
+        'https://lottosyndicatebucket.s3.eu-west-1.amazonaws.com/games/e17751d4-set-for-life-monday-draw-results-live-national-lottery.jpg',
+        2
+    ), (
+        'Thunderball',
+        '2023-08-16',
+        500000.0,
+        'https://lottosyndicatebucket.s3.eu-west-1.amazonaws.com/games/UK-Thunderball-logo.jpg',
+        1
+    ), (
+        'lotto hotpicks',
+        '2023-08-16',
+        3500000.0,
+        'https://lottosyndicatebucket.s3.eu-west-1.amazonaws.com/games/Euromillions-Hotpicks-logo.jpg',
+        2
+    ), (
+        'Euro Millions hotpicks',
+        '2023-08-18',
+        10000000,
+        'https://lottosyndicatebucket.s3.eu-west-1.amazonaws.com/games/Euromillions-Hotpicks-logo.jpg',
+        1.50
+    ), (
+        'lotto',
+        '2023-08-16',
+        20000000,
+        'https://lottosyndicatebucket.s3.eu-west-1.amazonaws.com/games/lotto-numbers-live-triple-rollover-May-25-results-evg-1616045.jpg',
+        1
+    );
 
+INSERT INTO
+    games (
+        maximum_players,
+        treasury,
+        syndicate_id,
+        game_type_id
+    )
+VALUES (5, 0, 1, 1), (2, 0, 2, 2), (7, 0, 3, 3);
 
--- INSERT INTO user_syndicate_reviews (created_date, title, content, user_id, syndicate_id) VALUES
---   ('2023-07-19', 'Great team!', 'Terrible, im leaving a bad review', 1, 1),
+INSERT INTO
+    user_syndicate_reviews (
+        created_date,
+        title,
+        content,
+        user_id,
+        syndicate_id
+    )
+VALUES (
+        '2023-07-19',
+        'Great team!',
+        'The Thunderbolts really know what they''re doing!',
+        1,
+        1
+    ), (
+        '2023-07-20',
+        'Sneaky but Effective',
+        'The Silent Shadows have a unique approach, but it pays off!',
+        2,
+        2
+    ), (
+        '2023-07-21',
+        'Warriors Indeed!',
+        'Code Warriors live up to their name!',
+        3,
+        3
+    );
 
+INSERT INTO
+    ticket_status (name)
+VALUES ('win'), ('loss'), ('pending');
 
--- INSERT INTO ticket_status (name) VALUES
--- ('win'),
--- ('loss'),
--- ('pending');
+INSERT INTO
+    user_games (
+        deposit,
+        start_date,
+        role_id,
+        game_id,
+        user_id
+    )
+VALUES
+(0, '2023-07-21', 3, 1, 1);
 
--- INSERT INTO game_user_syndicates_ticket (ticket_code, total_reward_value, ticket_status_id, user_syndicate_id, game_id) VALUES
--- (234546, 0, 3, 1, 1),
--- (234355, 0, 3, 2, 2),
--- (2345621, 0, 3, 3, 3);
+INSERT INTO
+    game_user_game_ticket (
+        ticket_code,
+        total_reward_value,
+        ticket_status_id,
+        game_id
+    )
+VALUES ('123456', 0, 3, 1), ('234567', 0, 3, 2), ('345678', 0, 3, 3);
 
--- INSERT INTO boards (name, syndicate_id) VALUES
--- ('terrible talk', 1),
--- ('fan club', 2),
--- ('powerball chat', 3);
+INSERT INTO
+    boards (name, game_id)
+VALUES ('Thunderbolts Talk', 1), ('Shadows Speak', 2), ('Warriors Word', 3);
 
--- INSERT INTO board_message (message, created_date, board_id, user_syndicate_id) VALUES
--- ('hi how is everyone!', '2023-07-22', 1, 1),
--- ('great how are you buddy.', '2023-07-23', 2, 2),
--- ('we have to keep moving ', '2023-07-24', 3, 3);
-
+INSERT INTO
+    board_message (
+        message,
+        created_date,
+        board_id,
+        user_game_id
+    )
+VALUES (
+        'Lets win this, Thunderbolts!',
+        '2023-07-22',
+        1,
+        1
+    ), (
+        'We move silently, we win always.',
+        '2023-07-23',
+        2,
+        1
+    ), (
+        'No bug can stop us!',
+        '2023-07-24',
+        3,
+        1
+    );
