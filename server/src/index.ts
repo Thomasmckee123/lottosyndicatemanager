257107373968ffcdd0ac5fd038018ed7c824ac3b
Thomasmckee123 <thomas.mckee@unosquare.com>
2023-09-07 10:42:57 +0100

    index change

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,5 +2,6 @@ import {app} from "./app";
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+
+console.log(`Server listening on ${PORT} AWS ACCESS KEY${process.env.ACCESS_KEY} AWS SECRET${process.env.SECRET_ACCESS_KEY}`);
 });
